dd6750f12d038af1404796cb4b5d373e7a2fb821
Leo Gaab <leonelgaab@gmail.com>
2019-12-02 21:36:27 -0300

    Updated Filter component

--- src/app/components/product-list/product-filter/product-filter.component.ts
+++ src/app/components/product-list/product-filter/product-filter.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../product-list.service';
 
 @Component({
   selector: 'app-product-filter',
@@ -6,22 +7,100 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./product-filter.component.css']
 })
 export class ProductFilterComponent implements OnInit {
+  @Input() productList: Product[];
+  @Output() filteredList: EventEmitter<Product[]> = new EventEmitter();
 
   displayFilters = false;
+  searchText = '';
+  property = 'name';
+  direction: 'asc' | 'desc' | 'none' = 'none';
+  quantity = 0;
+  minPrice = 0;
+  maxPrice = 50000;
+  available = '1';
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  filterProducts(value: string) {
-    // console.log(event.target.value);
-    // this.test.filter((x: Product) => x.name.includes(state.name) && x.availability == state.availability);
+  applyFilter() {
+    const availability = this.available === '1' || false;
+    const filterList = this.productList.filter( product => {
+      return product.name.includes(this.searchText) &&
+        product.quantity >= this.quantity &&
+        +product.price >= this.minPrice && +product.price <= this.maxPrice
+        && product.availability === availability;
+    });
+
+    this.filteredList.emit(this.sortBy(filterList, this.property, this.direction));
+  }
+
+  filterSearchBox(event) {
+    this.searchText = event.target.value;
+  }
+
+  filterQuantity(event) {
+    const value = event.target.value || 0;
+    this.quantity = value;
+  }
+
+  filterMin(event) {
+    this.minPrice = event.target.value || 0;
+  }
+
+  filterMax(event) {
+    this.maxPrice = event.target.value || 0;
+  }
+
+  filterAvailability(event) {
+    this.available = event.target.value;
+  }
+
+  sortBy(array: any[], property: string, direction: 'asc' | 'desc' | 'none' ) {
+    if ( direction === 'none') {
+      return array;
+    }
+
+    return array.sort( (a , b) => {
+      const aProperty = a[property];
+      const bProperty = b[property];
+
+      if (aProperty > bProperty) {
+        return direction === 'desc' ? 1 : -1;
+      }
+      if (aProperty < bProperty) {
+        return direction === 'desc' ? -1 : 1;
+      }
+      return 0;
+    });
+  }
+
+  setSortDirection(direction) {
+    this.direction = direction;
+
+    this.applyFilter();
   }
 
   openFilters() {
-    console.log('clicked');
     this.displayFilters = !this.displayFilters;
+  }
+
+  cancel() {
+    this.resetFilters();
+    this.displayFilters = !this.displayFilters;
+  }
+
+  resetFilters() {
+    this.searchText = '';
+    this.property = 'name';
+    this.direction = 'none';
+    this.quantity = 0;
+    this.minPrice = 0;
+    this.maxPrice = 50000;
+    this.available = '1';
+
+    this.filteredList.emit(this.productList);
   }
 
 }
